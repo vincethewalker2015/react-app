c3db5fa6d504269ab6d234b766f7a15ee4015e8c
Vincent Walker <vincethewalker@yahoo.co.uk>
2023-10-17 09:50:51 +0100

    Added routing navbar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
+import { BrowserRouter as Router } from "react-router-dom"
+// "react-router-dom" is an npm package that enables you to implement dynamic routing in a web app (need to run npm i react-router-dom after adding this above )
 import './App.css'
-import PostsList from "./features/posts/PostsList";
+// import PostsList from "./features/posts/PostsList"; - Removed as we use AppRoutes
+import NavBar from "./componants/NavBar";
+import AppRoutes from "./componants/AppRoutes";
 function App() {
   return (
-    <>
-     <div className='app'></div>
-      <h1>React on Rails Blog</h1>
-      <p>Find this application layout in client/src/App.jsx</p>
-      <PostsList />
-    </>
+    // Adding a Router comp to route to pages (as in rails)
+    <Router>
+      <div className='app'>
+        <h1>React on Rails Blog</h1>
+        <p>Find this application layout in client/src/App.jsx</p>
+        <NavBar></NavBar>
+         {/* <PostsList /> This now gets replaced by AppRoutes*/}
+         <AppRoutes />
+      </div>
+    </Router>
   )
 }
 
